88e426e9174530a514ebf8807b2affbc8d983dd8
SteffanoP <steffanoxpereira@gmail.com>
2021-11-14 23:34:28 -0300

    refactor: function to create a pandas DataFrame

--- src/generate.py
+++ src/generate.py
@@ -9,21 +9,15 @@ from sklearn.datasets import make_classification
 def blobs(samples, centers, features):
     X, y = make_blobs(n_samples=samples, centers=centers, 
                         n_features=features)
-    df = pd.DataFrame(X)
-    df['label'] = y
-    return df
+    return _create_pd_dataframe(X, y)
 
 def moons(samples, noise):
     X, y = make_moons(n_samples=samples, noise=noise)
-    df = pd.DataFrame(X)
-    df['label'] = y
-    return df
+    return _create_pd_dataframe(X, y)
 
 def circles(samples, noise):
     X, y = make_circles(n_samples=samples, noise=noise)
-    df = pd.DataFrame(X)
-    df['label'] = y
-    return df
+    return _create_pd_dataframe(X, y)
 
 def classification(samples, features):
     X, y = make_classification(
@@ -33,6 +27,9 @@ def classification(samples, features):
         n_informative=2, 
         n_clusters_per_class=1
         )
-    df = pd.DataFrame(X)
-    df['label'] = y
+    return _create_pd_dataframe(X, y)
+
+def _create_pd_dataframe(samples, label):
+    df = pd.DataFrame(samples)
+    df['label'] = label
     return df
