b3d7e549b08f6e65636e24d0dcb2fb96f741f72a
SteffanoP <steffanoxpereira@gmail.com>
2021-11-14 23:23:53 -0300

    feat: dataset maker for classification problems

--- src/generate.py
+++ src/generate.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_moons
 from sklearn.datasets import make_circles
+from sklearn.datasets import make_classification
 
 def blobs(samples, centers, features):
     X, y = make_blobs(n_samples=samples, centers=centers, 
@@ -20,6 +21,18 @@ def moons(samples, noise):
 
 def circles(samples, noise):
     X, y = make_circles(n_samples=samples, noise=noise)
+    df = pd.DataFrame(X)
+    df['label'] = y
+    return df
+
+def classification(samples, features):
+    X, y = make_classification(
+        n_samples=samples, 
+        n_features=features, 
+        n_redundant=0, 
+        n_informative=2, 
+        n_clusters_per_class=1
+        )
     df = pd.DataFrame(X)
     df['label'] = y
     return df
