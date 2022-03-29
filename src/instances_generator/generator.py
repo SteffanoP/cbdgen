c50a1b4e9cfb129fdeb2cf5c8c892556838d504d
Steffano Pereira <steffanoxpereira@gmail.com>
2022-03-29 13:12:18 -0300

    refactor: hide unnecessary variables

--- src/instances_generator/generator.py
+++ src/instances_generator/generator.py
@@ -10,32 +10,32 @@ class InstancesGenerator:
         5: '_mlabel_classf'
     }
 
-    def __init__(self, options: dict) -> DataFrame:
-        self.samples = options['samples']
-        self.attributes = options['attributes']
-        self.classes = options['classes']
-        self.optional_option = options['maker'][1]
+    def __init__(self, options: dict):
+        self._samples = options['samples']
+        self._attributes = options['attributes']
+        self._classes = options['classes']
+        self._optional_option = options['maker'][1]
 
     def generate(self, type_gen: int) -> DataFrame:
         return getattr(self, self.types_generator.get(type_gen))()
 
     def _blobs(self) -> DataFrame:
-        centers = self.optional_option
-        return maker.blobs(self.samples, centers, self.attributes)
+        centers = self._optional_option
+        return maker.blobs(self._samples, centers, self._attributes)
 
     def _moons(self) -> DataFrame:
-        noise = self.optional_option
-        return maker.moons(self.samples, noise)
+        noise = self._optional_option
+        return maker.moons(self._samples, noise)
 
     def _circles(self) -> DataFrame:
-        noise = self.optional_option
-        return maker.circles(self.samples, noise)
+        noise = self._optional_option
+        return maker.circles(self._samples, noise)
 
     def _classf(self) -> DataFrame:
-        return maker.classification(self.samples, self.attributes, 
-                                    self.classes)
+        return maker.classification(self._samples, self._attributes, 
+                                    self._classes)
 
     def _mlabel_classf(self) -> DataFrame:
-        labels = self.optional_option
-        return maker.multilabel_classification(self.samples, self.attributes, 
-                                               self.classes, labels)
+        labels = self._optional_option
+        return maker.multilabel_classification(self._samples, self._attributes, 
+                                               self._classes, labels)
